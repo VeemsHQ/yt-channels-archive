b02a3e50e62c1434601b204884bd7111a287a65d
Richard O'Dwyer <richard@richard.do>
2021-01-23 17:21:17 +0000

    fix

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open('HISTORY.md', 'r', encoding='utf-8') as hist_file:
 
 setup(
     name='yt-channels-archive',
-    version='#',
+    version='0.1.7.dev0',
     packages=find_packages(exclude=['tests*']),
     include_package_data=True,
     zip_safe=False,
